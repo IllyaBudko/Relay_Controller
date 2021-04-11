629d1410d872944d393f9b3199c22a74aace312f
IllyaBudko <35385161+IllyaBudko@users.noreply.github.com>
2021-04-11 17:28:25 -0600

    Initial commit on master branch
    - Clean project, with project configs

---
+++ Objects/main.d
@@ -0,0 +1,32 @@
+.\objects\main.o: RTE\Device\STM32F030F4Px\STCubeGenerated\Src\main.c
+.\objects\main.o: .\RTE\Device\STM32F030F4Px\STCubeGenerated\Inc\main.h
+.\objects\main.o: C:\Users\Illya Budko\AppData\Local\Arm\Packs\Keil\STM32F0xx_DFP\2.1.0\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal.h
+.\objects\main.o: .\RTE\Device\STM32F030F4Px\STCubeGenerated\Inc\stm32f0xx_hal_conf.h
+.\objects\main.o: C:\Users\Illya Budko\AppData\Local\Arm\Packs\Keil\STM32F0xx_DFP\2.1.0\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_rcc.h
+.\objects\main.o: C:\Users\Illya Budko\AppData\Local\Arm\Packs\Keil\STM32F0xx_DFP\2.1.0\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_def.h
+.\objects\main.o: C:\Users\Illya Budko\AppData\Local\Arm\Packs\Keil\STM32F0xx_DFP\2.1.0\Drivers\CMSIS\Device\ST\STM32F0xx\Include\stm32f0xx.h
+.\objects\main.o: C:\Users\Illya Budko\AppData\Local\Arm\Packs\Keil\STM32F0xx_DFP\2.1.0\Drivers\CMSIS\Device\ST\STM32F0xx\Include\stm32f030x6.h
+.\objects\main.o: C:\Users\Illya Budko\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\core_cm0.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: C:\Users\Illya Budko\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\main.o: C:\Users\Illya Budko\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\main.o: C:\Users\Illya Budko\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\main.o: C:\Users\Illya Budko\AppData\Local\Arm\Packs\Keil\STM32F0xx_DFP\2.1.0\Drivers\CMSIS\Device\ST\STM32F0xx\Include\system_stm32f0xx.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+.\objects\main.o: C:\Users\Illya Budko\AppData\Local\Arm\Packs\Keil\STM32F0xx_DFP\2.1.0\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_rcc_ex.h
+.\objects\main.o: C:\Users\Illya Budko\AppData\Local\Arm\Packs\Keil\STM32F0xx_DFP\2.1.0\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_gpio.h
+.\objects\main.o: C:\Users\Illya Budko\AppData\Local\Arm\Packs\Keil\STM32F0xx_DFP\2.1.0\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_gpio_ex.h
+.\objects\main.o: C:\Users\Illya Budko\AppData\Local\Arm\Packs\Keil\STM32F0xx_DFP\2.1.0\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_exti.h
+.\objects\main.o: C:\Users\Illya Budko\AppData\Local\Arm\Packs\Keil\STM32F0xx_DFP\2.1.0\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_dma.h
+.\objects\main.o: C:\Users\Illya Budko\AppData\Local\Arm\Packs\Keil\STM32F0xx_DFP\2.1.0\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_dma_ex.h
+.\objects\main.o: C:\Users\Illya Budko\AppData\Local\Arm\Packs\Keil\STM32F0xx_DFP\2.1.0\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_cortex.h
+.\objects\main.o: C:\Users\Illya Budko\AppData\Local\Arm\Packs\Keil\STM32F0xx_DFP\2.1.0\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_flash.h
+.\objects\main.o: C:\Users\Illya Budko\AppData\Local\Arm\Packs\Keil\STM32F0xx_DFP\2.1.0\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_flash_ex.h
+.\objects\main.o: C:\Users\Illya Budko\AppData\Local\Arm\Packs\Keil\STM32F0xx_DFP\2.1.0\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_i2c.h
+.\objects\main.o: C:\Users\Illya Budko\AppData\Local\Arm\Packs\Keil\STM32F0xx_DFP\2.1.0\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_i2c_ex.h
+.\objects\main.o: C:\Users\Illya Budko\AppData\Local\Arm\Packs\Keil\STM32F0xx_DFP\2.1.0\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_pwr.h
+.\objects\main.o: C:\Users\Illya Budko\AppData\Local\Arm\Packs\Keil\STM32F0xx_DFP\2.1.0\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_pwr_ex.h
+.\objects\main.o: C:\Users\Illya Budko\AppData\Local\Arm\Packs\Keil\STM32F0xx_DFP\2.1.0\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_tim.h
+.\objects\main.o: C:\Users\Illya Budko\AppData\Local\Arm\Packs\Keil\STM32F0xx_DFP\2.1.0\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_tim_ex.h
+.\objects\main.o: C:\Users\Illya Budko\AppData\Local\Arm\Packs\Keil\STM32F0xx_DFP\2.1.0\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_uart.h
+.\objects\main.o: C:\Users\Illya Budko\AppData\Local\Arm\Packs\Keil\STM32F0xx_DFP\2.1.0\Drivers\STM32F0xx_HAL_Driver\Inc\stm32f0xx_hal_uart_ex.h
---
+++ Objects/startup_stm32f030x6.d
@@ -0,0 +1,1 @@
+.\objects\startup_stm32f030x6.o: C:\Embedded\Serious_WorkSpace\Relay_Controller\RTE\Device\STM32F030F4Px\STCubeGenerated\MDK-ARM\startup_stm32f030x6.s
